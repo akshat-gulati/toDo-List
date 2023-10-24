d2082e55a62c0bfd6a0e9c4a03b514eab92fd8f5
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-10-24 23:23:56 +0530

    2 way binding done in title

--- app/page.js
+++ app/page.js
@@ -1,18 +1,29 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const page = () => {
+  const [title, settitle] = useState("")
+  const [desc, setdesc] = useState("")
   return (
     <>
-    <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>Akshat's ToDo List</h1>
+    <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>
+      Akshat's ToDo List
+      </h1>
     <form>
       <input 
       type="text"
       className='text 2xl: border-zinc-800 border-4 m-8 px-4 py-2'
-      placeholder="Enter task here: "/>
-      
-      <input type="text"
+      placeholder="Enter Title here: "
+      value={title}
+      onChange={(e)=>{
+        settitle(e.target.value)
+      }}
+      />
+      <input 
+      type="text"
        className='text 2xl: border-zinc-800 border-4 m-8 px-4 py-2'
-      placeholder="Enter description here: "/>
+      placeholder="Enter description here: "
+      />
       <button className='bg-black text-white px-4 py-2 text-2xl font-bold rounded m-5'>Add Task</button>
       
     </form>
