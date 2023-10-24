d71a10ac694dc96765064fa02ec5035d60711414
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-10-25 01:02:23 +0530

    2 way binding done in description

--- app/page.js
+++ app/page.js
@@ -4,12 +4,16 @@ import React, { useState } from 'react'
 const page = () => {
   const [title, settitle] = useState("")
   const [desc, setdesc] = useState("")
+
+  const submitHandler = ()=>{
+    console.log("hahahahah")
+  }
   return (
     <>
     <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>
       Akshat's ToDo List
       </h1>
-    <form>
+    <form onSubmit={submitHandler}>
       <input 
       type="text"
       className='text 2xl: border-zinc-800 border-4 m-8 px-4 py-2'
@@ -23,6 +27,8 @@ const page = () => {
       type="text"
        className='text 2xl: border-zinc-800 border-4 m-8 px-4 py-2'
       placeholder="Enter description here: "
+      value={desc}
+      onChange={(e)=>{setdesc(e.target.value)}}
       />
       <button className='bg-black text-white px-4 py-2 text-2xl font-bold rounded m-5'>Add Task</button>
       
