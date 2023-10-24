306edbaf49bfd31bc15ae98a29c8e40bc4fca776
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-10-25 01:29:13 +0530

    content of textinput added to rendertask

--- app/page.js
+++ app/page.js
@@ -4,10 +4,27 @@ import React, { useState } from 'react'
 const page = () => {
   const [title, settitle] = useState("")
   const [desc, setdesc] = useState("")
+  const [mainTask, setMainTask] = useState([])
 
-  const submitHandler = ()=>{
-    console.log("hahahahah")
+  const submitHandler = (e)=>{
+    e.preventDefault()
+    setMainTask([...mainTask, {title, desc}])
+    settitle("")
+    setdesc("")
+    console.log(mainTask)
   }
+
+  let renderTask = <h2>NO Task Available</h2>
+  renderTask= mainTask.map((t,i)=>{
+    return (
+      <li>
+        <div className='flex justify-between mb-5'>
+      <h5 className='text-2xl font-semibold'>{t.title}</h5>
+      <h6 className='text-xl font-semibold'>{t.desc}</h6>
+    </div>
+      </li>
+    )
+  })
   return (
     <>
     <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>
@@ -33,6 +50,12 @@ const page = () => {
       <button className='bg-black text-white px-4 py-2 text-2xl font-bold rounded m-5'>Add Task</button>
       
     </form>
+    <hr />
+    <div className='p-8 bg-slate-200'>
+      <ul>
+        {renderTask}
+      </ul>
+    </div>
     </>
   )
 }
