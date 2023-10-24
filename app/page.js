a29baf6183cf74c7cc0d287c2fe0eb6e6a8bbdc8
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-10-24 23:11:31 +0530

    type here and and type description here and add task button added

--- app/page.js
+++ app/page.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <h1 className='bg-black text-white p-5 text-5xl font-bold text-center'>Akshat's ToDo List</h1>
+    <form>
+      <input 
+      type="text"
+      className='text 2xl: border-zinc-800 border-4 m-8 px-4 py-2'
+      placeholder="Enter task here: "/>
+      
+      <input type="text"
+       className='text 2xl: border-zinc-800 border-4 m-8 px-4 py-2'
+      placeholder="Enter description here: "/>
+      <button className='bg-black text-white px-4 py-2 text-2xl font-bold rounded m-5'>Add Task</button>
+      
+    </form>
+    </>
   )
 }
 
